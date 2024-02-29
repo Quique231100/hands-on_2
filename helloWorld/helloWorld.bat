71c9d740dad9e6a414305956057a4c42da86fe94
Quique231100 <130946779+Quique231100@users.noreply.github.com>
2024-02-28 22:24:38 -0600

    Update helloWorld.bat

--- helloWorld/helloWorld.bat
+++ helloWorld/helloWorld.bat
@@ -1,1 +1,34 @@
+@echo off
+echo Creando archivo myText.txt...
+echo Hola Mundo > myText.txt
+echo Proceso completado...
+pause
+echo.
+echo Contenido del archivo myText.txt
+type myText.txt
+pause
+echo.
+echo Creando folder backup
+mkdir backup
+pause
+echo.
+echo Copiando archivo myText.txt a backup...
+copy myText.txt backup\myText.txt
+echo Archivo Copiado exitosamente...
+pause
+echo.
+echo Listando archivos de backup...
+cd backup
+dir
+pause
+echo.
+echo Eliminando myText.txt...
+del myText.txt
+pause
+echo.
+echo Eliminando subdir backup
+cd ..
+rmdir /s /q backup
+echo backup eliminado...
+pause
 
